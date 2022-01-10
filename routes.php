c95833a242baca26b6e76e3efdee41096dcc4da7
jessy2022 <97400317+jessy2022@users.noreply.github.com>
2022-01-10 17:01:40 +0300

    Add files via upload

---
+++ routes.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>ONLINE BUS RESERVATION</title>
+<link rel="stylesheet" type="text/css" href="xres/css/style.css" />
+<link rel="icon" type="image/png" href="xres/images/favicon.png" />
+<link type="text/css" href="css/styles.css" rel="stylesheet" media="all" />
+</head>
+
+<body>
+<div id="wrapper">
+	<div id="header">
+    <h5><a href="index.php"><img src="xres/images/logos.png" class="logo" alt="ONLINE BUS RESERVATION" /></a></h5>
+        <ul id="mainnav">
+			<li><a href="index.php">Home</a></li>
+            <li><a href="gallery.php">Gallery</a></li>
+            <li><a href="history.php">History</a></li>
+            <li class="current"><a href="routes.php">Routes</a></li>
+            <li><a href="location.php">location</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+    	</ul>
+	</div>
+    <div id="content">
+    	<div id="gallerycontainer">
+			<div class="portfolio-area" style="margin:0 auto; padding:140px 20px 20px 20px; width:820px;">	
+					<table cellspacing="0" cellpadding="0" border="0" style="width: 449px;"><colgroup><col width="136"> <col width="179"> <col width="134"> </colgroup>
+<tbody>
+<tr>
+<td width="136" height="24" class="xl65"><span style="color: #000000;">ORIGIN:</span></td>
+<td width="179" class="xl66"><span style="color: #000000;">KAMPALA,&nbsp; UGANDA</span></td>
+<td width="134" class="xl67">&nbsp;</td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl66"><span style="color: #000000;">DESTINATION</span></td>
+<td width="179" class="xl67"><span style="color: #000000;">DEPARTURE</span></td>
+<td width="134" class="xl67"><span style="color: #000000;">TYPE</span></td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl69"><span style="color: #000000;">KABALE</span></td>
+<td width="179" class="xl70"><span style="color: #000000;">6:00PM</span></td>
+<td width="134" class="xl71"><span style="color: #000000;">DE LUXE</span></td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl69"><span style="color: #000000;">KIGALI/RWANDA</span></td>
+<td width="179" class="xl70"><span style="color: #000000;">6:30PM</span></td>
+<td width="134" class="xl71"><span style="color: #000000;">SUPER DE LUXE</span></td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl69"><span style="color: #000000;">KISORO</span></td>
+<td width="179" class="xl70"><span style="color: #000000;">7:00PM</span></td>
+<td width="134" class="xl71"><span style="color: #000000;">RECONDITIONED</span></td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl69"><span style="color: #000000;">MBARARA</span></td>
+<td width="179" class="xl68"><span style="color: #000000;">7:15PM</span></td>
+<td width="134" class="xl71"><span style="color: #000000;">DE LUXE</span></td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl69"><span style="color: #000000;">RUKUNGIRI</span></td>
+<td width="179" class="xl68"><span style="color: #000000;">7:30PM</span></td>
+<td width="134" class="xl71"><span style="color: #000000;">SUPER DE LUXE</span></td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl69"><span style="color: #000000;">BUNAGANA</span></td>
+<td width="179" class="xl68"><span style="color: #000000;">7:45PM</span></td>
+<td width="134" class="xl71"><span style="color: #000000;">RECONDITIONED</span></td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl69"><span style="color: #000000;">GOMA/DRC</span></td>
+<td width="179" class="xl68"><span style="color: #000000;">8:00PM</span></td>
+<td width="134" class="xl71"><span style="color: #000000;">SUPER DE LUXE</span></td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl69"><span style="color: #000000;">KANUNGU</span></td>
+<td width="179" class="xl68"><span style="color: #000000;">8:20PM</span></td>
+<td width="134" class="xl71"><span style="color: #000000;">RECONDITIONED</span></td>
+</tr>
+
+<tr>
+<td width="136" height="22" class="xl69">&nbsp;</td>
+<td width="179" class="xl68">&nbsp;</td>
+<td width="134" class="xl68">&nbsp;</td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl69">&nbsp;</td>
+<td width="179" class="xl68">&nbsp;</td>
+<td width="134" class="xl68">&nbsp;</td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl65"><span style="color: #000000;">ORIGIN:</span></td>
+<td width="179" class="xl66"><span style="color: #000000;">KABALE TERMINAL</span></td>
+<td width="134" class="xl67">&nbsp;</td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl66"><span style="color: #000000;">DESTINATION</span></td>
+<td width="179" class="xl67"><span style="color: #000000;">DEPARTURE</span></td>
+<td width="134" class="xl67"><span style="color: #000000;">TYPE</span></td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl69"><span style="color: #000000;">KAMPALA</span></td>
+<td width="179" class="xl68"><span style="color: #000000;">6:30PM</span></td>
+<td width="134" class="xl71"><span style="color: #000000;">DELUXE</span></td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl69"><span style="color: #000000;">MBARARA</span></td>
+<td width="179" class="xl68"><span style="color: #000000;">7:30PM</span></td>
+<td width="134" class="xl71"><span style="color: #000000;">DELUXE</span></td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl69"><span style="color: #000000;">KISORO/span></td>
+<td width="179" class="xl68"><span style="color: #000000;">8:30PM</span></td>
+<td width="134" class="xl71"><span style="color: #000000;">DELUXE</span></td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl69"><span style="color: #000000;">GOMA/DRC</span></td>
+<td width="179" class="xl68"><span style="color: #000000;">10:30PM</span></td>
+<td width="134" class="xl71"><span style="color: #000000;">DELUXE</span></td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl69"><span style="color: #000000;">KIGALI/RWANDA</span></td>
+<td width="179" class="xl68"><span style="color: #000000;">7:00PM</span></td>
+<td width="134" class="xl71"><span style="color: #000000;">SUPER DELUXE</span></td>
+</tr>
+
+<tr>
+<td width="136" height="22" class="xl69">&nbsp;</td>
+<td width="179" class="xl68">&nbsp;</td>
+<td width="134" class="xl71">&nbsp;</td>
+</tr>
+<tr>
+<td height="22">&nbsp;</td>
+<td width="179" class="xl68">&nbsp;</td>
+<td width="134" class="xl71">&nbsp;</td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl65"><span style="color: #000000;">ORIGIN:</span></td>
+<td width="179" class="xl66"><span style="color: #000000;">MBARARA TERMINAL</span></td>
+<td width="134" class="xl67">&nbsp;</td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl66"><span style="color: #000000;">DESTINATION</span></td>
+<td width="179" class="xl67"><span style="color: #000000;">DEPARTURE</span></td>
+<td width="134" class="xl67"><span style="color: #000000;">TYPE</span></td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl69"><span style="color: #000000;">KAMPALA</span></td>
+<td width="179" class="xl68"><span style="color: #000000;">3:00PM</span></td>
+<td width="134" class="xl71"><span style="color: #000000;">DELUXE</span></td>
+</tr>
+<tr>
+<td width="136" height="22" class="xl69"><span style="color: #000000;">KABALE</span></td>
+<td width="179" class="xl68"><span style="color: #000000;">8:00PM</span></td>
+<td width="134" class="xl71"><span style="color: #000000;">DELUXE</span></td>
+</tr>
+<tr>
+<td height="22"><span style="color: #000000;">KISORO</span></td>
+<td width="179" class="xl68"><span style="color: #000000;">5:00PM</span></td>
+<td width="134" class="xl71"><span style="color: #000000;">AIRCONDITIONED</span></td>
+</tr>
+<tr>
+<td height="20">&nbsp;</td>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+</tr>
+</tbody>
+</table>
+				<div class="column-clear"></div>
+            </div>
+			<div class="clearfix"></div>
+        </div>
+    </div>
+    
+
+
+
+<div id="footer">
+    <div id="footer">
+    <h4>+256(0)392234567 &bull; <a href="contact-us.php">Plot 19 Wilson Road, Travin House, Kampala – Uganda  </a></h4>
+    <p>Hours of Operation&nbsp;&nbsp;&bull;&nbsp;&nbsp;Mon - Sun: 06:00 am - 12:00 am</p>
+    <a href="index.php"><img src="xres/images/footer-logos.jpg" alt="ONLINE BUS RESERVATION" /></a>
+    <p>&copy; Copyright 2020 Gateway Bus Services Limited |<br /></p>
+</div>
+
+</div>
+</body>
+</html>
