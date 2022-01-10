cbe56bd3a55e199269ad0ad30cd4b296df6a8fc8
jessy2022 <97400317+jessy2022@users.noreply.github.com>
2022-01-10 16:58:43 +0300

    Add files via upload

---
+++ history.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>ONLINE BUS RESERVATION</title>
+<link rel="stylesheet" type="text/css" href="xres/css/style.css" />
+<link rel="icon" type="image/png" href="xres/images/favicon.png" />
+<link type="text/css" href="css/styles.css" rel="stylesheet" media="all" />
+</head>
+
+<body>
+<div id="wrapper">
+	<div id="header">
+    <h5><a href="index.php"><img src="xres/images/logos.png" class="logo" alt="James Buchanan Pub and Restaurant" /></a></h5>
+        <ul id="mainnav">
+			<li><a href="index.php">Home</a></li>
+            <li><a href="gallery.php">Gallery</a></li>
+            <li class="current"><a href="history.php">History</a></li>
+            <li><a href="routes.php">Routes</a></li>
+            <li><a href="location.php">location</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+    	</ul>
+	</div>
+    <div id="content">
+    	<div id="gallerycontainer">
+			<div class="portfolio-area" style="margin:0 auto; padding:140px 20px 20px 20px; width:820px;">	
+					Gateway Bus Services Limited is a domestic bus company offering public transport business activities in Uganda especially the Southwestern region. The travel company’s main office is situated on Plot19 Wilson Road, Travin House, Kampala – Uganda. 
+					<br>
+					<br>
+					<b>Online Bus Ticket Reservation System</b> 
+					The online bus reservation ticket, often called e-ticket, is the digital ticket equivalent of a paper ticket. The term is most commonly associated with web-based issued tickets. 
+					<br>
+					<br>
+					Online Bus Ticket Reservation Systems incorporate bus schedules, fare tariffs, passenger reservations and ticket records. A company’s direct distribution works within its main reservation system, as well as pushing out information to its smaller distribution systems. 
+					<br>
+					<br>
+					The second distribution channel is where consumers use the internet or mobile applications to make their own reservations and all messaging is transmitted by a standardized messaging system for real time interactive communication and for informational and booking type of messages.
+					<br>
+					<br>
+					At Gateway Bus Services Limited, we AIM to give you TOTAL SATISFACTION! What are you guys waiting for? Spread the word and let us pamper you with what we've got!
+					<br>
+					<br>
+					We are open daily all day, all night. SEE YOU THERE!
+					<br>
+				<div class="column-clear"></div>
+            </div>
+			<div class="clearfix"></div>
+        </div>
+    </div>
+    
+
+
+
+<div id="footer">
+	<div id="footer"><br />
+	<h4>+256(0)392234567 &bull; <a href="contact-us.php">Plot 19 Wilson Road, Travin House, Kampala – Uganda  </a></h4>
+	<p>Hours of Operation&nbsp;&nbsp;&bull;&nbsp;&nbsp;Mon - Sun: 06:00 am - 12:00 am</p>
+	<a href="index.php"><img src="xres/images/footer-logos.jpg" alt="ONLINE BUS RESERVATION" /></a>
+	<p>&copy; Copyright 2020 Gateway Bus Services Limited |<br /></p>
+</div>
+
+</div>
+</body>
+</html>
